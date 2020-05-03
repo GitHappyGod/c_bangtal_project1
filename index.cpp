eec1600b0eff0f6fb603b589fab6f49ff2fe486c
HappyGod <chami0205@gmail.com>
2020-05-03 21:29:23 +0900

    second

--- index.cpp
+++ index.cpp
@@ -3,15 +3,17 @@
 
 SceneID scene1, scene2, scene3, scene4, scene5;
 
-ObjectID problem1, problem2;
+ObjectID problem1, problem2, problem3;
 
 ObjectID left[5];
 ObjectID right[5];
+TimerID timer1;
 
-int leftX[5] = {169, 22};
-int rightX[5] = {472, 336};
-int Y[5] = {226, 130};
-int radius[5] = {20, 100};
+int leftX[5] = {169, 22, 140};
+int rightX[5] = {472, 350, 436};
+int Y[5] = {226, 303, 332};
+int radius[5] = {20, 50, 30};
+bool c1 = false, c2 = false, c3 = false;
 
 bool checkIn(int x, int y, int cx, int cy, int r) {
 	return (x > cx - r && x < cx + r && y > cy - r && y < cy + r);
@@ -19,24 +21,62 @@ bool checkIn(int x, int y, int cx, int cy, int r) {
 
 void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
 	if (object == problem1) {
-		if(checkIn(x, y, leftX[0], Y[0], radius[0]) || checkIn(x, y, rightX[0], Y[0], radius[0])) {
-			showObject(left[0]);
-			showObject(right[0]);
-			showMessage("정답입니다!!");
+		if (c1) {
+			enterScene(scene2);
+		}
+		else {
+			if (checkIn(x, y, leftX[0], Y[0], radius[0]) || checkIn(x, y, rightX[0], Y[0], radius[0])) {
+				showObject(left[0]);
+				showObject(right[0]);
+				showMessage("정답입니다!! 클릭하면 다음문제로 넘어갑니다.");
+				c1 = true;
+			}
+		}
+	}
+	else if (object == problem2) {
+		printf("%d %d \n", x, y);
+		if (c2) {
+			enterScene(scene3);
+		}
+		else {
+			if (checkIn(x, y, leftX[1], Y[1], radius[1]) || checkIn(x, y, rightX[1], Y[1], radius[1])) {
+				showObject(left[1]);
+				showObject(right[1]);
+				showMessage("정답입니다!! 클릭하면 다음문제로 넘어갑니다.");
+				c2 = true;
+			}
+		}
+	}
+	else if (object == problem3) {
+		printf("%d %d \n", x, y);
+		if (c3) {
 			endGame();
 		}
+		else {
+			if (checkIn(x, y, leftX[2], Y[2], radius[2]) || checkIn(x, y, rightX[2], Y[2], radius[2])) {
+				showObject(left[2]);
+				showObject(right[2]);
+				showMessage("정답입니다!! 시간내에 모든 문제를 맞추셨습니다. 클릭하시면 게임이 종료됩니다.");
+				c3 = true;
+			}
+		}
+	}
+}
+
+void timerCallback(TimerID timer) {
+	if (timer1 == timer1) {
+		endGame();
 	}
 }
 
 int main() {
 
 	setMouseCallback(mouseCallback);
+	setTimerCallback(timerCallback);
 
 	scene1 = createScene("틀린그림찾기1", "");
 	scene2 = createScene("틀린그림찾기2", "");
 	scene3 = createScene("틀린그림찾기3", "");
-	scene4 = createScene("틀린그림찾기4", "");
-	scene5 = createScene("틀린그림찾기5", "");
 
 	problem1 = createObject("images/part1.jpg");
 	locateObject(problem1, scene1, 0, 0);
@@ -48,11 +88,35 @@ int main() {
 	locateObject(right[0], scene1, rightX[0] - radius[0], Y[0] - radius[0]);
 
 
-	/* problem2 = createScene("images/part2.jpg");
+	problem2 = createObject("images/part2.jpg");
+	locateObject(problem2, scene2, 0, 0);
+	showObject(problem2); // 22 336 130
+
+	left[1] = createObject("images/check.png");
+	locateObject(left[1], scene2, leftX[1] - radius[1], Y[1] - radius[1]);
+	right[1] = createObject("images/check.png");
+	locateObject(right[1], scene2, rightX[1] - radius[1], Y[1] - radius[1]);
+
+
+	problem3 = createObject("images/part3.jpg");
+	locateObject(problem3, scene3, 0, 0);
+	showObject(problem3);
+
+	left[2] = createObject("images/check.png");
+	locateObject(left[2], scene3, leftX[2] - radius[2], Y[2] - radius[2]);
+	right[2] = createObject("images/check.png");
+	locateObject(right[2], scene3, rightX[2] - radius[2], Y[2] - radius[2]);
+
+	 /*problem2 = createScene("images/part2.jpg");
 	locateObject(problem2, scene2, 0, 0); // 22 336 130
 	showObject(problem2); */
 
 	showMessage("좌우에 틀린 곳을 찾아보세요");
+
+
+	timer1 = createTimer(30.0f);
+	showTimer(timer1);
+	startTimer(timer1);
 
 	startGame(scene1);
 }
